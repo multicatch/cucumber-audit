676977d3472af31df71c2a07940b1208ef610f26
multicatch <cezaryre@gmail.com>
2020-04-20 23:06:34 +0200

    Fix Python version in runserver.sh

--- cucumber-audit-scenarios/src/test/resources/exampleApp/runserver.sh
+++ cucumber-audit-scenarios/src/test/resources/exampleApp/runserver.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 virtualenv venv
 source venv/bin/activate
-pip install -r requirements.txt
-python manage.py runserver --noreload --nothreading &
+pip3 install -r requirements.txt
+python3 manage.py runserver --noreload --nothreading &
 DJANGO_SERVER_PID=$!
 read -r
 kill $DJANGO_SERVER_PID
