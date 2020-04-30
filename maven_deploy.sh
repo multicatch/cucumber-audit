4c4c9ba444ebee543b5141ae4cee8bdb6e6f5e1f
multicatch <cezaryre@gmail.com>
2020-04-30 17:23:16 +0200

    Deployment configuration

---
+++ maven_deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+openssl aes-256-cbc -K $encrypted_930808c487b2_key -iv $encrypted_930808c487b2_iv -in deployment/signingkey.asc.enc -out deployment/signingkey.asc -d
+gpg --fast-import deployment/signingkey.asc
+mvn deploy -P build-deployment --settings deployment/settings.xml
