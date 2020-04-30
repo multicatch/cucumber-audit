d7a514d544194b2cd7bff3a6c11aae72991a3f10
multicatch <cezaryre@gmail.com>
2020-04-30 19:25:31 +0200

    Updated gpg key import method

--- maven_deploy.sh
+++ maven_deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 openssl aes-256-cbc -K $encrypted_930808c487b2_key -iv $encrypted_930808c487b2_iv -in deployment/signingkey.asc.enc -out deployment/signingkey.asc -d
-echo "$GPG_PASSPHRASE" | gpg --fast-import deployment/signingkey.asc
+echo "$GPG_PASSPHRASE" | gpg --batch --yes --no-tty --passphrase-fd 0 --fast-import deployment/signingkey.asc
 mvn deploy -P build-deployment --settings deployment/settings.xml
