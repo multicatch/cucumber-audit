a67235cdf76742dd115330e081318d558eb650c4
multicatch <cezaryre@gmail.com>
2020-06-21 13:54:31 +0200

    Update chrome driver for Travis

--- download_drivers.sh
+++ download_drivers.sh
@@ -3,6 +3,6 @@ mkdir webdrivers
 cd webdrivers
 wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz
 tar xzvf geckodriver-*.tar.gz
-wget https://chromedriver.storage.googleapis.com/81.0.4044.69/chromedriver_linux64.zip
+wget https://chromedriver.storage.googleapis.com/83.0.4103.39/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 cd ..
