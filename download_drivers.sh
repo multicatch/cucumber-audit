9e998add825832216bad92e24263df1ab9914438
multicatch <cezaryre@gmail.com>
2021-04-21 18:57:33 +0200

    Setup GitHub Actions build

--- download_drivers.sh
+++ download_drivers.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+set -ex
+# install chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+# install drivers
 mkdir webdrivers
 cd webdrivers
 wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz
